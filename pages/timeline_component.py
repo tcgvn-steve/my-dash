8e09ec22c4744ccb56f1c09de4608da83db727e4
tcgvn-steve <steve@capogrecogroup.com>
2021-10-13 11:03:38 +0700

    Timeline Page

--- pages/timeline_component.py
+++ pages/timeline_component.py
@@ -5,50 +5,65 @@ from dash.dependencies import Input, Output, State
 from custom_components import TimelineComponentClass
 import dash_bootstrap_components as dbc
 items = [{
-    "title": "Steve",
-    "cardTitle": "Probination",
+    "title": "2021",
+    "cardTitle": "Steve",
     "cardSubtitle": "Steve is interested in hitting the gym and playing soccer",
     "cardDetailedText": "Men of the British Expeditionary Force (BEF) wade out to..",
     "media": {
          "type": "IMAGE",
          "source": {
-             "url": "http://someurl/image.jpg"
+             "url": "/assets/avatar.jpg"
          }
          }
 }, {
-    "title": "Ned",
-    "cardTitle": "Staff",
+    "title": "2020",
+    "cardTitle": "Ned",
     "cardSubtitle": "Ned fancies playing video games",
     "cardDetailedText": "Men of the British Expeditionary Force (BEF) wade out to..",
     "media": {
         "type": "IMAGE",
         "source": {
-             "url": "http://someurl/image.jpg"
+             "url": "/assets/avatar2.png"
         }
     }
 }, {
-    "title": "Jacob",
-    "cardTitle": "Staff",
+    "title": "2019",
+    "cardTitle": "Jacob",
     "cardSubtitle": "Jacob desires gaming gear",
     "cardDetailedText": "Men of the British Expeditionary Force (BEF) wade out to..",
     "media": {
         "type": "IMAGE",
         "source": {
-             "url": "http://someurl/image.jpg"
+             "url": "/assets/avatar.jpg"
         }
     }
 }
 ]
+
 layout = dbc.Container([
     dbc.Row([
         dbc.Col(TimelineComponentClass(
             id='input', items=items, mode='VERTICAL_ALTERNATING'
         ), className="d-inline-block col-md-8"),
-        dbc.Col(id='output', className="d-inline-block col-md-4"),
+        dbc.Col(id='output', children="", className="d-inline-block col-md-4"),
     ])
 ])
 
 
 @app.callback(Output('output', 'children'), [Input('input', 'value')])
 def display_output(value):
-    return 'You have entered {}'.format(value)
+    res = items[0]
+    if value:
+        print("=== value ", type(value))
+        print("=== value ", (value))
+        import json
+        res = json.loads(value)
+        print("=== res ", (res))
+        print("=== res ", (res['cardTitle']))
+
+    return html.Div([
+        html.H2(children=("{}: {}".format(
+            res['title'], res['cardTitle']))),
+        dbc.Col(children="{}. {}".format(
+            res['cardSubtitle'], res['cardDetailedText']))
+    ])
